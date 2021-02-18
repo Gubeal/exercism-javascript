39c27d60a45ddc678db4284d498120924c872df9
Celso <celsogubeal@gmail.com>
2021-02-18 10:07:07 -0300

    Solve Darts

--- darts/darts.js
+++ darts/darts.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  throw new Error('Remove this statement and implement this function');
+export const score = (xDart=0, yDart=0) => {
+  let xCenterCircle = 0;
+  let yCenterCircle = 0;
+  let outerCircle = 100;
+  let middleCircle = 25;
+  let innerCircle = 1;
+  let point = (xDart - xCenterCircle) * (xDart - xCenterCircle) + (yDart - yCenterCircle) * (yDart - yCenterCircle);
+  if (point <= innerCircle) {
+    return 10;
+  } else if (point <= middleCircle) {
+    return 5;
+  } else if (point <= outerCircle) {
+    return 1
+  }
+  return 0;
 };
