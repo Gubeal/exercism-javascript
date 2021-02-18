e599549a85d74e00ff969c4f6c0ddb7d3b5fdc16
Celso <celsogubeal@gmail.com>
2021-02-18 18:15:35 -0300

    Solve Resistor Color Duo

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,10 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decodedValue = (colors) => {
+  const MAX_DECIMALS = 2;
+  let sum = '';
+  for (let i = 0; i < MAX_DECIMALS; i++) {
+    sum += COLORS.indexOf(colors[i])
+  }
+  return parseInt(sum);
 };
+
+export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
