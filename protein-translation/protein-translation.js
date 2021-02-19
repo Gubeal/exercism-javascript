3f93a05896a95f546cdc2b572b5449dc6708fab4
Celso <celsogubeal@gmail.com>
2021-02-19 19:55:20 -0300

    Solve protein translation

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -1,8 +1,41 @@
-//
-// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const NUCLEOTIDE_SIZE = 3;
+const stopCodons = new Set(['UAA', 'UAG', 'UGA']);
+const toProtein = new Map([
+  ['AUG', 'Methionine'],
+  ['UUU', 'Phenylalanine'],
+  ['UUC', 'Phenylalanine'],
+  ['UUA', 'Leucine'],
+  ['UUG', 'Leucine'],
+  ['UCU', 'Serine'],
+  ['UCC', 'Serine'],
+  ['UCA', 'Serine'],
+  ['UCG', 'Serine'],
+  ['UAU', 'Tyrosine'],
+  ['UAC', 'Tyrosine'],
+  ['UGU', 'Cysteine'],
+  ['UGC', 'Cysteine'],
+  ['UGG', 'Tryptophan'],
+]);
 
-export const translate = () => {
-  throw new Error('Remove this statement and implement this function');
+function splitNucleotides(rna=''){
+  let piece = rna;
+  let nuclei = [];
+
+  for (let i = 0; i < (rna.length / NUCLEOTIDE_SIZE); i++){
+    nuclei.push(piece.slice(0, NUCLEOTIDE_SIZE));
+    piece = piece.slice(NUCLEOTIDE_SIZE, piece.length);
+  }
+  return nuclei;
+};
+
+export const translate = (rna='') => {
+  let proteins = [];
+  let nuclei = splitNucleotides(rna);
+  
+  for (let nucle of nuclei){
+    if (stopCodons.has(nucle)) break;
+    if (!toProtein.has(nucle)) throw new Error('Invalid codon');
+    proteins.push(toProtein.get(nucle));
+  }
+  return proteins;
 };
