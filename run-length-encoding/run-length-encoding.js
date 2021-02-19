7b05a382f1e13d253dc71fc9ca1e440a84487815
Celso <celsogubeal@gmail.com>
2021-02-19 09:57:30 -0300

    Solve run length encoding

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -1,12 +1,54 @@
-//
-// This is only a SKELETON file for the 'Run Length Encoding' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const encode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const encode = (msg='') => {
+  const LETTERS = /^[A-Z ]+$/;
+  let result = msg;
+  if (LETTERS.test(msg.toUpperCase())) {
+    result = '';
+    let ref = '';
+    let count = 0;
+    for (let i = 0; i <= msg.length; i++){
+      if (i == 0) ref = msg[i];
+      if (ref == msg[i]) {
+        count++;
+      } else {
+        if (count == 1) {
+          result += ref;
+        } else {
+          result += count + ref;
+        }
+        count = 1;
+        ref = msg[i];
+      }
+    }
+  }
+  return result;
 };
 
-export const decode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decode = (msg='') => {
+  const NUMBERS = /^[0-9]+$/;
+  const NUMBERS_LETTERS = /^[0-9A-Z ]+$/;
+  let result = msg;
+  
+  if (NUMBERS_LETTERS.test(msg.toUpperCase())){
+    result = '';
+    let numberLetters = '';
+    for (let i = 0; i<msg.length; i++) {
+      if (NUMBERS.test(msg[i])){
+        numberLetters += msg[i];
+      } else {
+        if (i == 0) {
+          result += msg[i];
+        } else {
+          if (NUMBERS.test(msg[i-1])) {
+            for (let c = 0; c<parseInt(numberLetters); c++) {
+              result += msg[i];
+            }
+            numberLetters = '';
+          } else {
+            result += msg[i];
+          }
+        }
+      }
+    }
+  }
+  return result;
 };
