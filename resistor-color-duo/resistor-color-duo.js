045043b1baa02f0595337bfffb36b7dc23cadcd1
Celso <celsogubeal@gmail.com>
2021-02-19 10:06:16 -0300

    Improved resistor color duo

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,10 +1,5 @@
 export const decodedValue = (colors) => {
-  const MAX_DECIMALS = 2;
-  let sum = '';
-  for (let i = 0; i < MAX_DECIMALS; i++) {
-    sum += COLORS.indexOf(colors[i])
-  }
-  return parseInt(sum);
+  return Number(colors.map( colorValues => COLORS.indexOf(colorValues)).slice(0,2).join(''));
 };
 
 export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
