cfded749124089cfd2bbe97f4a28f2078964d5ac
Celso <celsogubeal@gmail.com>
2021-02-23 10:15:24 -0300

    Solve Gigasecond

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+  const GIGAMILISECOND = 1000000000000;
+  let lastDate = new Date(GIGAMILISECOND + date.getTime());
+  return new Date(Date.parse(lastDate.toISOString()));
 };
