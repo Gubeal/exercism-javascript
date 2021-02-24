a1645bef39b2e67dee6d6d1313995062c88d5b01
Celso <celsogubeal@gmail.com>
2021-02-23 21:10:00 -0300

    Solve Clock

--- clock/clock.js
+++ clock/clock.js
@@ -1,26 +1,46 @@
-//
-// This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const HOURS_DAY = 24;
+const MINUTES_HOUR = 60;
+const ZERO = 0;
+const ONES_MAX_DECIMAL = 9;
 
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(hour=ZERO, minute=ZERO) {
+      this.hour = hour;
+      this.minute = minute;
+  }
+
+  intHours(hours) {
+    return (hours >= HOURS_DAY) ? this.intHours(hours - HOURS_DAY) : (hours < ZERO) ? this.intHours(hours + HOURS_DAY) : hours;
+  }
+
+  intMinutes(hours, minutes) {
+    return (minutes >= MINUTES_HOUR) ? this.intMinutes(hours + 1, minutes - MINUTES_HOUR) : (minutes < ZERO) ? this.intMinutes(hours - 1, minutes + MINUTES_HOUR) : {hours, minutes};
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    let timeSpend = this.intMinutes(ZERO, this.minute);
+    let hours = this.intHours(this.hour + timeSpend.hours);
+
+    hours = (hours > ONES_MAX_DECIMAL)? hours : `0${hours}`;
+    let minutes = (timeSpend.minutes > ONES_MAX_DECIMAL) ? timeSpend.minutes : `0${timeSpend.minutes}`;
+    return `${hours}:${minutes}`;
   }
 
-  plus() {
-    throw new Error('Remove this statement and implement this function');
+  plus(minute=ZERO) {
+    let timeSpend = this.intMinutes(ZERO, minute);
+    this.hour += Number(timeSpend.hours);
+    this.minute += Number(timeSpend.minutes);
+    return this;
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(minute=ZERO) {
+    let timeSpend = this.intMinutes(ZERO, -Math.abs(minute));
+    this.hour += Number(timeSpend.hours);
+    this.minute += Number(timeSpend.minutes);
+    return this;
   }
 
-  equals() {
-    throw new Error('Remove this statement and implement this function');
+  equals(time) {
+    return this.toString() == time.toString();
   }
 }
