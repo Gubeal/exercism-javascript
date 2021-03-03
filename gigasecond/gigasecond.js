ee2dbcc8adaed785601eb4252b6e9901f0847f3b
Celso <celsogubeal@gmail.com>
2021-03-03 00:13:26 -0300

    Improved Gigasecond

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,10 +1,5 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const GIGAMILISECOND = 1000000000000;
 
 export const gigasecond = (date) => {
-  const GIGAMILISECOND = 1000000000000;
-  let lastDate = new Date(GIGAMILISECOND + date.getTime());
-  return new Date(Date.parse(lastDate.toISOString()));
+  return new Date(Date.parse(new Date(GIGAMILISECOND + date.getTime()).toISOString()));
 };
